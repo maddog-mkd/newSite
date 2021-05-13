553f73983834cfbb8f0897ecf86c6f8a7f97586b
Rade <you@example.com>
2021-05-14 01:18:17 +0200

    update

--- js/main.js
+++ js/main.js
@@ -4,3 +4,27 @@ const navbar = document.querySelector('.navbar');
 menuIcon.addEventListener('click', () => {
 	navbar.classList.toggle('change');
 });
+
+
+
+
+
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+	const triggerBottom = window.innerHeight / 5 * 4
+
+	boxes.forEach(box => {
+		const boxTop = box.getBoundingClientRect().top
+
+		if(boxTop < triggerBottom) {
+			box.classList.add('show')
+		} else {
+			box.classList.remove('show')
+		}
+	})
+}
