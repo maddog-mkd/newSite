bd25fb903ffb84a361cf4765850aeea70dc036a8
Rade <you@example.com>
2021-05-14 22:56:12 +0200

    update-modal

--- js/main.js
+++ js/main.js
@@ -85,3 +85,14 @@ setInterval(updateTestimonial, 30000)
 
 
 
+
+// MODAL
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false);
